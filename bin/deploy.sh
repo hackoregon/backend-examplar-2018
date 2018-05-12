c54f99ce6dd6322729b75b137d6b556c2a0750ce
Nathan Miller <nmiller217@gmail.com>
2018-05-12 05:03:54 -0700

    change $DOCKER_PATH to $REMOTE_DOCKER_PATH for better clarity use $REMOTE_DOCKER_PATH as argument to ecs-deploy.sh as well change docker image tag to "travis-buildnum-XX" (remove git branch)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,25 +12,25 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         echo Getting the ECR login...
         eval $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
 
-        DOCKER_PATH="$DOCKER_REPO"/"$DOCKER_REPO_NAMESPACE"/"$PRODUCTION_DOCKER_IMAGE"
+        REMOTE_DOCKER_PATH="$DOCKER_REPO"/"$DOCKER_REPO_NAMESPACE"/"$PRODUCTION_DOCKER_IMAGE"
         
         # tag with branch and travis build number then push
-        TAG="$TRAVIS_BRANCH"-"$TRAVIS_BUILD_NUMBER"
+        TAG=travis-buildnum-"$TRAVIS_BUILD_NUMBER"
         echo Tagging with "$TAG"
-        docker tag "$PRODUCTION_DOCKER_IMAGE":latest "$DOCKER_PATH":"$TAG"    
-        docker push "$DOCKER_PATH":"$TAG"
+        docker tag "$PRODUCTION_DOCKER_IMAGE":latest "$REMOTE_DOCKER_PATH":"$TAG"    
+        docker push "$REMOTE_DOCKER_PATH":"$TAG"
 
         # tag with "latest" then push
         TAG=latest
         echo Tagging with "$TAG"
-        docker tag "$PRODUCTION_DOCKER_IMAGE":latest "$DOCKER_PATH":"$TAG"
-        docker push "$DOCKER_PATH":"$TAG"
+        docker tag "$PRODUCTION_DOCKER_IMAGE":latest "$REMOTE_DOCKER_PATH":"$TAG"
+        docker push "$REMOTE_DOCKER_PATH":"$TAG"
         
         #echo Running ecs-deploy.sh script...
         bin/ecs-deploy.sh  \
            --service-name "$ECS_SERVICE_NAME" \
            --cluster "$ECS_CLUSTER"   \
-           --image "$DOCKER_REPO"/"$DOCKER_REPO_NAMESPACE"/"$PRODUCTION_DOCKER_IMAGE":latest \
+           --image "$REMOTE_DOCKER_PATH":latest \
            --timeout 300
     #else
     #    echo "Skipping deploy because branch is not master"
