a9d3fbe092b358e599bcfe9c29bd42755036a03f
Mike Lonergan <mikethecanuck@gmail.com>
2018-05-24 11:12:40 -0400

    removed boilerplate examples, moved hard-coded env vars to appropriate place, and explained the usage of PROJECT_NAME

--- bin/get-ssm-parameters.sh
+++ bin/get-ssm-parameters.sh
@@ -5,19 +5,13 @@
 
 # Modelled on https://aws.amazon.com/blogs/compute/managing-secrets-for-amazon-ecs-applications-using-parameter-store-and-iam-roles-for-tasks/
 
-EC2_REGION = "us-west-2" # unfortunately cannot rely on the env var values that this script is meant to pull in
-PROJECT_NAME = "disaster-resilience"
-
-#APP1_WITH_ENCRYPTION=`aws ssm get-parameters --names prod.app1.db-pass --with-decryption --region $EC2_REGION --output text 2>&1`
-#APP1_WITHOUT_ENCRYPTION=`aws ssm get-parameters --names prod.app1.db-pass --no-with-decryption --region $EC2_REGION --output text 2>&1`
-#LICENSE_WITH_ENCRYPTION=`aws ssm get-parameters --names general.license-code --with-decryption --region $EC2_REGION --output text 2>&1`
-#LICENSE_WITHOUT_ENCRYPTION=`aws ssm get-parameters --names general.license-code --no-with-decryption --region $EC2_REGION --output text 2>&1`
-#APP2_WITHOUT_ENCRYPTION=`aws ssm get-parameters --names prod.app2.user-name --no-with-decryption --region $EC2_REGION --output text 2>&1`
-
 # TODO:
 # 1. determine the values of the prefixes for each of the Parameter Store parameters (e.g. /production/2018/) that pass into the --names flag
 # 2. echo out the parameter values so they can be used by the container apps
 # 3. determine the best place to run this script so that it populates the env vars before they're needed by other code
+
+EC2_REGION = "us-west-2" # unfortunately cannot rely on dynamic env var values that this script is meant to pull in
+PROJECT_NAME = "disaster-resilience" # must be set to each project's "Final naming convention" from here https://github.com/hackoregon/civic-devops/issues/1
 
 # Get unencrypted values
 POSTGRES_HOST=`aws ssm get-parameters --names "/production/2018/API/$PROJECT_NAME/POSTGRES_HOST" --no-with-decryption --region $EC2_REGION --output text 2>&1`
