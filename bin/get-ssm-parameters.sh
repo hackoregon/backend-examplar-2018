117b6f2c0d46fae4c22cfc1400decf13248fa4f9
Mike Lonergan <mikethecanuck@gmail.com>
2018-05-24 00:10:11 -0400

    untested script that should set the environment variables from values obtained from SSM

--- bin/get-ssm-parameters.sh
+++ bin/get-ssm-parameters.sh
@@ -5,7 +5,8 @@
 
 # Modelled on https://aws.amazon.com/blogs/compute/managing-secrets-for-amazon-ecs-applications-using-parameter-store-and-iam-roles-for-tasks/
 
-EC2-REGION = "us-west-2" # unfortunately cannot rely on the env var values that this script is meant to pull in
+EC2_REGION = "us-west-2" # unfortunately cannot rely on the env var values that this script is meant to pull in
+PROJECT_NAME = "disaster-resilience"
 
 #APP1_WITH_ENCRYPTION=`aws ssm get-parameters --names prod.app1.db-pass --with-decryption --region $EC2_REGION --output text 2>&1`
 #APP1_WITHOUT_ENCRYPTION=`aws ssm get-parameters --names prod.app1.db-pass --no-with-decryption --region $EC2_REGION --output text 2>&1`
@@ -17,3 +18,21 @@ EC2-REGION = "us-west-2" # unfortunately cannot rely on the env var values that 
 # 1. determine the values of the prefixes for each of the Parameter Store parameters (e.g. /production/2018/) that pass into the --names flag
 # 2. echo out the parameter values so they can be used by the container apps
 # 3. determine the best place to run this script so that it populates the env vars before they're needed by other code
+
+# Get unencrypted values
+POSTGRES_HOST=`aws ssm get-parameters --names "/production/2018/API/$PROJECT_NAME/POSTGRES_HOST" --no-with-decryption --region $EC2_REGION --output text 2>&1`
+POSTGRES_NAME=`aws ssm get-parameters --names "/production/2018/API/$PROJECT_NAME/POSTGRES_NAME" --no-with-decryption --region $EC2_REGION --output text 2>&1`
+POSTGRES_PORT=`aws ssm get-parameters --names "/production/2018/API/$PROJECT_NAME/POSTGRES_PORT" --no-with-decryption --region $EC2_REGION --output text 2>&1`
+POSTGRES_USER=`aws ssm get-parameters --names "/production/2018/API/$PROJECT_NAME/POSTGRES_USER" --no-with-decryption --region $EC2_REGION --output text 2>&1`
+
+# Get encrypted values
+DJANGO_SECRET_KEY=`aws ssm get-parameters --names "/production/2018/API/$PROJECT_NAME/DJANGO_SECRET_KEY" --with-decryption --region $EC2_REGION --output text 2>&1`
+POSTGRES_PASSWORD=`aws ssm get-parameters --names "/production/2018/API/$PROJECT_NAME/POSTGRES_PASSWORD" --with-decryption --region $EC2_REGION --output text 2>&1`
+
+# Set environment variables in the container
+export DJANGO_SECRET_KEY=$DJANGO_SECRET_KEY
+export POSTGRES_HOST=$POSTGRES_HOST
+export POSTGRES_NAME=$POSTGRES_NAME
+export POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+export POSTGRES_PORT=$POSTGRES_PORT
+export POSTGRES_USER=$POSTGRES_USER
