4633b5cbbd57ac1be90dc5147ab300a791e524da
Mike Lonergan <mikethecanuck@gmail.com>
2018-05-26 11:38:53 -0700

    call the script to pull in Parameter Store values as environment variables

--- bin/production-docker-entrypoint.sh
+++ bin/production-docker-entrypoint.sh
@@ -6,6 +6,9 @@ set -e
 
 echo Debug: $DEBUG
 
+# Pull in environment variables values from AWS Parameter Store
+/code/bin/get-ssm-parameters.sh
+
 python -Wall manage.py collectstatic --noinput
 
 gunicorn $PROJECT_NAME.wsgi -c gunicorn_config.py
