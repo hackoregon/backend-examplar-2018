fab3505706219499c1b6dfd3f54dd24aa8bb841d
Nathan Miller <nmiller217@gmail.com>
2018-10-25 19:49:26 -0700

    use non-psycopg2 db driver to fix depecration warning

--- dead_songs/settings.py
+++ dead_songs/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'dead_songs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
